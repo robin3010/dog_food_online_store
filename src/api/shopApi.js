0fa770ce06f105822a417b4e185d781facf43514
robin3010 <robin3010@gmail.com>
2023-01-25 01:33:39 +0300

    additional fetch error handling condition

--- src/api/shopApi.js
+++ src/api/shopApi.js
@@ -4,6 +4,9 @@ class ShopApi {
   constructor({ baseUrl }) {
     this.baseUrl = baseUrl;
     this.authToken = '';
+    // this.userName = '';
+    // this.userId = '';
+    // this.userGroup = '';
   }
 
   setAuthToken(token) {
@@ -16,13 +19,16 @@ class ShopApi {
 
   checkFetchErrors() {
     if (this.status === 400) {
-      throw new Error('Некорректно заполнено одно из полей');
+      throw new Error('Некорректный запрос');
     }
     if (this.status === 401) {
       throw new Error('E-mail или пароль указаны неверно');
     }
     if (this.status === 409) {
       throw new Error('Пользователь с таким email уже существует');
+    }
+    if (this.status > 499) {
+      throw new Error(`${this.statusText}. Код ${this.status}`);
     }
     if (this.status > 401) {
       throw new Error(`Ошибка авторизации. Код ${this.status} ${this.statusText}`);
@@ -44,8 +50,13 @@ class ShopApi {
 
     this.checkFetchErrors.call(fetchSignIn);
 
-    const { token } = await fetchSignIn.json();
-    return token;
+    const response = await fetchSignIn.json();
+
+    // this.userName = response.data.name;
+    // this.userId = response.data._id;
+    // this.userGroup = response.data.group;
+
+    return response;
   }
 
   async signUp(userData) {
@@ -58,6 +69,28 @@ class ShopApi {
     });
 
     this.checkFetchErrors.call(fetchSignUp);
+  }
+
+  // пока не используется
+  async getUserInfo(group, property) {
+    this.checkAuthToken();
+
+    const fetchUserInfo = await fetch(`${this.baseUrl}/v2/${group}/users/me`, {
+      headers: {
+        authorization: this.getAuthHeader(),
+      },
+    });
+
+    // this.checkFetchErrors.call(fetchUserInfo);
+
+    const userInfo = await fetchUserInfo.json();
+
+    if (property) {
+      console.log(userInfo[property]);
+      return userInfo[property];
+    }
+    console.log(userInfo);
+    return userInfo;
   }
 
   async getGoodsList() {
