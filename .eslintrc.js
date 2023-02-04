c99c6124bc4696ab5cb9a432b27dfb7fcf8a4192
robin3010 <robin3010@gmail.com>
2023-02-04 21:42:39 +0300

    Revert "fixed autoimport doesn't work for installed packages"
    This reverts commit 7cb941deece5f703836d62274e4beecf2ac4e9eb.

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,14 +19,14 @@ module.exports = {
   plugins: [
     'react',
   ],
-  settings: {
-    'import/extensions': [
-      '.js',
-      '.jsx',
-      '.ts',
-      '.tsx',
-    ],
-  },
+  // settings: {
+  //   'import/extensions': [
+  //     '.js',
+  //     '.jsx',
+  //     '.ts',
+  //     '.tsx',
+  //   ],
+  // },
   rules: {
     // semi: ['error', 'never'],
     'no-console': 0,
